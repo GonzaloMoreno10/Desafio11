8ee6ebfa774281e645af216fb6dce4e6c4270536
GonzaloMoreno10 <63618513+GonzaloMoreno10@users.noreply.github.com>
2021-08-10 21:48:16 -0300

    commit 1 pug

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require('express-handlebars');
+const Handlebars  =  require('handlebars') ;
+const {allowInsecurePrototypeAccess}  =  require ( '@handlebars/allow-prototype-access' );
+//Inicializaciones
+const app = express();
+
+const publicPath = path.resolve(__dirname, '../public');
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','.pug');
+app.use(express.static(publicPath));
+
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
+
+app.use("/api/productos",require("./productosController"));
+
+
+//Listen
+app.listen(8080,(req,res)=>{
+    console.log("Servidor escuchando en el puerto 8080");
+})
